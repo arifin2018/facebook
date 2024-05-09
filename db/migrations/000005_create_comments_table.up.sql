266979ca728b1edb5f264b1dad6c8be66e077f92
arifinlenna <arifin@lenna.ai>
2024-05-10 00:53:27 +0700

    create table comments

---
+++ db/migrations/000005_create_comments_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS comments (
+    ID int NOT NULL AUTO_INCREMENT,
+    user_id int NOT NULL,
+    Post_id int NOT NULL,
+    Content text NOT NULL,
+    Created_at TIMESTAMP,
+    Updated_at TIMESTAMP,
+    Deleted_at TIMESTAMP,
+    PRIMARY KEY (ID)
+);
