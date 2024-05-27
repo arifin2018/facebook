4bbe169e3d34d1af5167da02d9900edcff062aa5
arifinlenna <arifin@lenna.ai>
2024-05-27 09:21:04 +0700

    Getenv("CODE_JWT")

--- helpers/auth/Jwt.go
+++ helpers/auth/Jwt.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"os"
+
 	"github.com/arifin2018/facebook/config"
 	"github.com/arifin2018/facebook/models"
 	"github.com/gofiber/fiber/v2"
@@ -39,7 +41,8 @@ func JWTClaims(f *fiber.Ctx, user *models.User) (string, error) {
 	// Create token
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	// Generate encoded token and send it as response.
-	t, err := token.SignedString([]byte("secret"))
+	var codeJwt = os.Getenv("CODE_JWT")
+	t, err := token.SignedString([]byte(codeJwt))
 	if err != nil {
 		return "", err
 	}
--- helpers/middlewares/JWTmiddleware.go
+++ helpers/middlewares/JWTmiddleware.go
@@ -1,12 +1,15 @@
 package middlewares
 
 import (
+	"os"
+
 	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
 )
 
 func JWTMiddlewareCheckAuth() fiber.Handler {
+	var codeJwt = os.Getenv("CODE_JWT")
 	return jwtware.New(jwtware.Config{
-		SigningKey: jwtware.SigningKey{Key: []byte("secret")},
+		SigningKey: jwtware.SigningKey{Key: []byte(codeJwt)},
 	})
 }
