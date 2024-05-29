4af25e0767d0d3da6a686427913dfccb0d5a09b1
arifinlenna <arifin@lenna.ai>
2024-05-29 09:44:11 +0700

    update origin

--- routes/router.go
+++ routes/router.go
@@ -12,7 +12,7 @@ import (
 func Router(app *fiber.App) {
 	app.Use(cors.New(cors.Config{
 		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
-		AllowOrigins:     "*",
+		AllowOrigins:     "https://myxml.in",
 		AllowCredentials: true,
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
