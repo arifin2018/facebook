aa979a4dc7b0592a5b4062f64354cdc5ae9ca0a1
arifinlenna <arifin@lenna.ai>
2024-05-29 12:27:39 +0700

    update cors

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	appFolder "github.com/arifin2018/facebook/config/app"
 	"github.com/arifin2018/facebook/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -12,6 +13,12 @@ func main() {
 	appFolder.InitProvider()
 	appFolder.InitProviderRequestValidator()
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 	config.Logger(app)
 	routes.Router(app)
 	app.Listen(":3000")
--- routes/router.go
+++ routes/router.go
@@ -6,11 +6,9 @@ import (
 	"github.com/arifin2018/facebook/helpers/auth"
 	"github.com/arifin2018/facebook/helpers/middlewares"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func Router(app *fiber.App) {
-	app.Use(cors.New())
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
