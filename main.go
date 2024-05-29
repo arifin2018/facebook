3b01505a44075c1cc59ba303068fb73ef57ebc0a
root <root@meet.lenna.ai>
2024-05-29 17:06:51 +0000

    success cors

--- main.go
+++ main.go
@@ -14,13 +14,13 @@ func main() {
 	appFolder.InitProviderRequestValidator()
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowHeaders: "Origin, Content-Type, Accept",
+/*		AllowHeaders: "Origin, Content-Type, Accept",
 		// AllowHeaders: "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
 		AllowOrigins: "http://localhost:5500,http://localhost:3000,https://rtc.lenna.ai",
 		// AllowOrigins:     "*",
 		AllowCredentials: false,
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
-	}))
+*/	}))
 	config.Logger(app)
 	routes.Router(app)
 	app.Listen(":3000")
